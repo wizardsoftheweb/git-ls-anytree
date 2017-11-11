f3058112bdfe096aa97b2596d4fc47ae1000ecf4
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-11 17:13:56 -0600

    Document convert_path_to_list

--- git-ls-anytree/convert_path_to_list.py
+++ git-ls-anytree/convert_path_to_list.py
@@ -1,5 +1,6 @@
 from os.path import split
 
 def convert_path_to_list(path_to_explode):
+    """Converts a path to a list exploded at the OS delimiters"""
     path_dirname, path_basename = split(path_to_explode)
     return (convert_path_to_list(path_dirname) if path_dirname else []) + [path_basename]

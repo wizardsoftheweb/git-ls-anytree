89e66ba5d8b8cbeb7739cb1b8871fe550518ba35
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-11 18:17:47 -0600

    Patch to prevent infinite recursion on root path

--- git_ls_anytree/convert_path_to_list.py
+++ git_ls_anytree/convert_path_to_list.py
@@ -3,4 +3,7 @@ from os.path import split
 def convert_path_to_list(path_to_explode):
     """Converts a path to a list exploded at the OS delimiters"""
     path_dirname, path_basename = split(path_to_explode)
-    return (convert_path_to_list(path_dirname) if path_dirname else []) + [path_basename]
+    if 0 < len(path_dirname) and '/' != path_dirname:
+        return convert_path_to_list(path_dirname) + [path_basename]
+    else:
+        return [path_basename]

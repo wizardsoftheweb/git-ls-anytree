4121322dcaf6e5eb45088251e817ba2854ed8c0b
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-12 15:51:15 -0600

    Force version and entry_points

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ from setuptools import setup
 with open(join('git_ls_anytree', 'VERSION')) as version_file:
     __version__ = version_file.read().strip()
 
-setup()
+setup(
+    version=__version__,
+    entry_points={
+        'console_scripts': [
+            'git-ls-anytree = git_ls_anytree.cli_file:cli'
+        ]
+    }
+)

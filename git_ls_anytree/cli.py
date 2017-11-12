d3e8ceead4030b0f20e081995b968935eee9900a
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-12 09:39:10 -0600

    Add abbrev and default-abbrev option

--- git_ls_anytree/cli.py
+++ git_ls_anytree/cli.py
@@ -18,6 +18,24 @@ def cli():
         help='Only print the tree structure per git-ls-tree --name-(only|status)'
     )
 
+    # TODO: Pull core.abbrev
+    parser.add_argument(
+        '--default-abbrev',
+        default=argparse.SUPPRESS,
+        dest='abbrev',
+        action='store_const',
+        const=7,
+        help='Equivalent to git-ls-tree --abbrev. Uses the default git short hash of seven characters.'
+    )
+
+    parser.add_argument(
+        '--abbrev',
+        nargs=1,
+        type=int,
+        default=argparse.SUPPRESS,
+        help='Sets the git object abbreviation per git-ls-tree --abbrev=n'
+    )
+
     parser.add_argument(
         '-F', '--classify',
         action='store_true',
@@ -39,32 +57,32 @@ def cli():
     )
 
     parser.add_argument(
-        'tree-ish',
+        'tree_ish',
+        metavar='tree-ish',
         nargs='?',
         type=str,
-        dest='tree_ish',
         default='HEAD',
         help='Reference to tree-ish'
     )
 
     parser.add_argument(
         'patterns',
-        dest='patterns',
         nargs='*',
         default=[],
         help='Subtrees within the main tree-ish'
     )
 
     args = parser.parse_args()
-    full_tree = GitLsTree(
-        tree_ish=args.tree_ish,
-        patterns=args.patterns,
-        trees_only=args.trees_only
-    )
-    full_tree.pretty_print(
-        name_only=args.name_only,
-        classify=args.classify
-    )
+    print args
+    # full_tree = GitLsTree(
+    #     tree_ish=args.tree_ish,
+    #     patterns=args.patterns,
+    #     trees_only=args.trees_only
+    # )
+    # full_tree.pretty_print(
+    #     name_only=args.name_only,
+    #     classify=args.classify
+    # )
 
 if __name__ == '__main__':
     cli()

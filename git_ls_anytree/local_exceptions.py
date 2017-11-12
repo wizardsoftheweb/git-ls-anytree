320722329308055010a2971b489bafb430b5e34e
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-11 23:55:49 -0600

    Provide a more verbose error message

--- git_ls_anytree/local_exceptions.py
+++ git_ls_anytree/local_exceptions.py
@@ -9,8 +9,8 @@ class BrokenTreeError(Exception):
     def __init__(self, owning_node, unknown_child):
         self.owning_node = owning_node
         self.unknown_child = unknown_child
-        full_path = owning_node.relative_path
-        self.msg = "The '%(full_path)s' tree does not have a '%(unknown_child)s' subtree or blob" % locals()
+        full_path = "'%s'" % (owning_node.relative_path) if owning_node.relative_path else 'root'
+        self.msg = "The %(full_path)s tree does not have a '%(unknown_child)s' subtree or blob" % locals()
 
     def __str__(self):
         return self.msg

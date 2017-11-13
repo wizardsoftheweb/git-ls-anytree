f2de8ee84b897b1bff9285272eb5724d36ecc046
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-12 18:59:51 -0600

    Attempt to set name and find packages in the py over cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from os.path import join
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(join('git_ls_anytree', 'VERSION')) as version_file:
     __version__ = version_file.read().strip()
 
 setup(
+    name='git_ls_anytree',
     version=__version__,
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'git_ls_anytree = git_ls_anytree.cli_file:cli'
+            'git-ls-anytree = git_ls_anytree.cli_file:cli'
         ]
     }
 )
